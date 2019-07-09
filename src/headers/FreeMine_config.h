f9c39355233efef7f4f356c7d168de2eb54f0be2
anton2920 <anton2920@gmail.com>
2019-07-09 22:51:45 +0300

    Small changes

--- src/headers/FreeMine_config.h
+++ src/headers/FreeMine_config.h
@@ -35,7 +35,7 @@
 #define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-/* #undef HAVE_STRINGS_H */
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
